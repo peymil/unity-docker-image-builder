5501ef9ff6a3aeabac007b16a182e56e92e06185
peymil <emrebadem_@hotmail.com>
2025-12-15 14:15:12 +0300

    Update dependencies and refactor logger configuration for improved performance

--- src/logger.ts
+++ src/logger.ts
@@ -1,31 +1,12 @@
 import pino from 'pino';
 
-const transports = [];
+const transport =
+  process.stdout.isTTY
+    ? { transport: { target: 'pino-pretty' } }
+    : {};
 
-if (process.env.LOKI_URL) {
-  transports.push({
-    target: 'pino-loki',
-    options: {
-      host: process.env.LOKI_URL,
-      labels: { app: 'unity-docker-image-builder' },
-      basicAuth: process.env.LOKI_USERNAME && process.env.LOKI_PASSWORD ? {
-        username: process.env.LOKI_USERNAME,
-        password: process.env.LOKI_PASSWORD
-      } : undefined,
-    }
-  });
-}
-
-transports.push({
-  target: 'pino-pretty',
-  options: {
-    colorize: true,
-  }
-});
 
 export const logger = pino({
   level: process.env.LOG_LEVEL || 'info',
-  transport: {
-    targets: transports
-  }
+  ...transport,
 });
